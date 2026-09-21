015b7b3acd3e1b7def48b42795087715eae0289c
Dip Narayan Chowdhury <dipnarayan.chowdhury7@gmail.com>
2026-09-21 20:04:57 +0530

    Add comments to HelloWorld program

--- src/javabasics/HelloWorld.java
+++ src/javabasics/HelloWorld.java
@@ -1,7 +1,20 @@
 package javabasics;
 
 public class HelloWorld {
+    /**
+     * This is main method
+     * @param args
+     * @author Dip
+     */
     public static void main(String[] args) {
+        // This is a comment
+
         System.out.println("Hello World");
+
+        /*
+        This is a
+        multiple line
+        comment
+         */
     }
 }
